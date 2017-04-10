9c4cb357303214f58efc32beff6fe3c368bd11b1
Ulka <gugik2006@gmail.com>
2017-04-10 21:26:26 +0300

    implementation of HW3 basic

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,20 +1,23 @@
 DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS users;
+
 DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq START 100000;
 
 CREATE TABLE users
 (
-  id         INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
-  name       VARCHAR NOT NULL,
-  email      VARCHAR NOT NULL,
-  password   VARCHAR NOT NULL,
-  registered TIMESTAMP DEFAULT now(),
-  enabled    BOOL DEFAULT TRUE,
+  id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  name             VARCHAR              NOT NULL,
+  email            VARCHAR              NOT NULL,
+  password         VARCHAR              NOT NULL,
+  registered       TIMESTAMP           DEFAULT now(),
+  enabled          BOOL                DEFAULT TRUE,
   calories_per_day INTEGER DEFAULT 2000 NOT NULL
 );
-CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
+CREATE UNIQUE INDEX users_unique_email_idx
+  ON users (email);
 
 CREATE TABLE user_roles
 (
@@ -22,4 +25,15 @@ CREATE TABLE user_roles
   role    VARCHAR,
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE meals
+(
+  user_id     INTEGER NOT NULL,
+  id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  dateTime    TIMESTAMP NOT NULL,
+  description VARCHAR NOT NULL,
+  calories    INTEGER NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+
 );
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,13 +1,23 @@
 DELETE FROM user_roles;
+DELETE FROM meals;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
-
 INSERT INTO users (name, email, password)
 VALUES ('User', 'user@yandex.ru', 'password');
-
 INSERT INTO users (name, email, password)
 VALUES ('Admin', 'admin@gmail.com', 'admin');
-
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+INSERT INTO meals (user_id, dateTime, description, calories) VALUES
+(100000, '2015-05-30 10:00:00.000000','Завтрак', '500'),
+(100000, '2015-05-30 13:00:00.000000','Обед', '800'),
+(100000, '2015-05-30 20:00:00.000000','Ужин', '500'),
+
+(100000, '2015-05-31 10:00:00.000000','Завтрак', '550'),
+(100000, '2015-05-31 13:00:00.000000', 'Обед', '500'),
+(100000, '2015-05-31 20:00:00.000000','Ужин', '1000'),
+
+(100001, '2015-05-30 10:00:00.000000','Завтрак', '700'),
+(100001, '2015-05-30 13:00:00.000000','Обед', '1200'),
+(100001, '2015-05-30 20:00:00.000000','Ужин', '2000');
